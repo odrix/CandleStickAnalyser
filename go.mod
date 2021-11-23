6263e5f5c49a6e96f42f45c7b5c8ec6514857604
odrix <adrilesage@gmail.com>
2021-11-23 23:20:58 +0100

    notify by mail with SendinBlue

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/binance-exchange/go-binance v0.0.0-20180518133450-1af034307da5
 	github.com/go-kit/kit v0.12.0
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/sendinblue/APIv3-go-library v2.0.0+incompatible
 )
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
 	"github.com/binance-exchange/go-binance"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"os"
+	sendinblue "github.com/sendinblue/APIv3-go-library/lib"
 )
 
 //const baseUrl string = "https://api.binance.com"
@@ -17,6 +19,108 @@ func main() {
 	logger = level.NewFilter(logger, level.AllowAll())
 	logger = log.With(logger, "time", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
 
+	pair := "BTCUSDT"
+	kl := getKline(logger, pair, binance.Day, 50)
+
+	var candlesDesc []Candle
+	for i := len(kl) - 1; i > 0; i-- {
+		candlesDesc = append(candlesDesc, Candle{kl[i]})
+	}
+
+	for i := 0; i < len(candlesDesc); i++ {
+		p := Pattern{}
+		if IsMorningStar(candlesDesc[i:]) {
+			p = Pattern{
+				Pair:  pair,
+				Type:  "Morning Star",
+				Start: candlesDesc[i-2].OpenTime,
+				End:   candlesDesc[i].CloseTime,
+			}
+		} else if IsEveningStar(candlesDesc[i:]) {
+			p = Pattern{
+				Pair:  pair,
+				Type:  "Evening Star",
+				Start: candlesDesc[i-2].OpenTime,
+				End:   candlesDesc[i].CloseTime,
+			}
+		} else if IsThreeWhiteSoldiers(candlesDesc[i:]) {
+			p = Pattern{
+				Pair:  pair,
+				Type:  "Three White Soldiers",
+				Start: candlesDesc[i-2].OpenTime,
+				End:   candlesDesc[i].CloseTime,
+			}
+		} else if IsThreeBlackCrows(candlesDesc[i:]) {
+			p = Pattern{
+				Pair:  pair,
+				Type:  "Three Black Crows",
+				Start: candlesDesc[i-2].OpenTime,
+				End:   candlesDesc[i].CloseTime,
+			}
+		}
+
+		if p.Type != "" {
+			trace(p)
+			notifyContacts(p)
+		}
+	}
+	fmt.Printf("end.")
+}
+
+func trace(pattern Pattern) {
+	fmt.Printf("%s on %s \n", pattern.Type, pattern.Start)
+}
+
+func notifyContacts(pattern Pattern) {
+
+	var ctx context.Context
+	cfg := sendinblue.NewConfiguration()
+	cfg.AddDefaultHeader("api-key", os.Getenv("SENDINBLUE_APIKEY"))
+
+	var contactsQuery = &sendinblue.ContactsApiGetContactsOpts{}
+
+	sib := sendinblue.NewAPIClient(cfg)
+	AllContacts, _, errContacts := sib.ContactsApi.GetContacts(ctx, contactsQuery)
+	if errContacts != nil {
+		fmt.Println("Error when calling get_contacts: ", errContacts.Error())
+		return
+	}
+
+	var templateParams interface{}
+	templateParams = map[string]interface{}{
+		"pair":    pattern.Pair,
+		"pattern": pattern.Type,
+	}
+
+	body := sendinblue.SendSmtpEmail{
+		To:         []sendinblue.SendSmtpEmailTo{},
+		Headers:    nil,
+		TemplateId: 5,
+		Params:     &templateParams,
+	}
+
+	for i := 0; i < len(AllContacts.Contacts); i++ {
+		body.To = append(body.To, sendinblue.SendSmtpEmailTo{Email: AllContacts.Contacts[i].Email})
+	}
+
+	email, _, err := sib.TransactionalEmailsApi.SendTransacEmail(ctx, body)
+
+	if err != nil {
+		fmt.Println("Error when calling AccountApi->get_account: ", err.Error())
+		return
+	}
+	fmt.Println("send template 5:", email)
+}
+
+func notifyTwitter(pattern Pattern) {
+
+}
+
+func notifyFacebook(pattern Pattern) {
+
+}
+
+func getKline(logger log.Logger, symbol string, interval binance.Interval, limit int) []*binance.Kline {
 	var ctx, _ = context.WithCancel(context.Background())
 
 	binanceService := binance.NewAPIService(
@@ -29,26 +133,12 @@ func main() {
 	b := binance.NewBinance(binanceService)
 
 	kl, err := b.Klines(binance.KlinesRequest{
-		Symbol:   "BTCUSDT",
-		Interval: binance.Day,
-		Limit:    300,
+		Symbol:   symbol,
+		Interval: interval,
+		Limit:    limit,
 	})
 	if err != nil {
 		panic(err)
 	}
-
-	var candlesDesc []Candle
-	for i := len(kl) - 1; i > 0; i-- {
-		candlesDesc = append(candlesDesc, Candle{kl[i]})
-	}
-
-	for i := 0; i < len(candlesDesc); i++ {
-		if IsMorningStar(candlesDesc[i:]) {
-			fmt.Printf("morning star on %s \n", candlesDesc[i].OpenTime)
-		} else if IsEveningStar(candlesDesc[i:]) {
-			fmt.Printf("evening star on %s \n", candlesDesc[i].OpenTime)
-		}
-	}
-	fmt.Printf("end.")
-
+	return kl
 }
